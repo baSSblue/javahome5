87a0d7c05e08e60365d84c0068945628ea77a728
Bella <bella.kallen2014@gmail.com>
2023-10-29 02:56:10 +0300

    scr added

--- src/main/java/ru/netology/services/StatisticsService.java
+++ src/main/java/ru/netology/services/StatisticsService.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class StatisticsService {
+package ru.netology.services;
+
+public class StatisticsService {
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int count = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money = money - expenses;
+                money = money / 3;
+                count++;
+            } else {
+                money = money + income;
+                money = money - expenses;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/ru/netology/services/StatisticsServiceTest.java
+++ src/test/java/ru/netology/services/StatisticsServiceTest.java
@@ -1,4 +1,43 @@
 
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 public class StatisticsServiceTest {
 
+    @Test
+    void testMoneysCase1() {
+        StatisticsService service = new StatisticsService();
+
+        int expected = 3;
+
+        int actual = service.calculate(10_000, 3_000, 20_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void testMoneyCase2() {
+        StatisticsService service = new StatisticsService();
+
+        int expected = 2;
+
+        int actual = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (files = "src/test/resources/data_test.csv")
+        public void testParams(int income, int expenses, int threshold, int expected){
+        StatisticsService service = new StatisticsService();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+        }
 }
